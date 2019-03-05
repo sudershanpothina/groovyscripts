6df2a0b676e60cbd4ad21e52ffa795d9a923a84a
Sudershan Pothina <Sdrshn-Home@sudershans-mbp.lan>
2019-03-05 06:24:42 -0600

    adding dotnet ci

---
+++ vars/dotnet.groovy
@@ -0,0 +1,15 @@
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    node {
+        deleteDir()
+        stage('checkout') {
+            checkout scm
+        }
+    }
+
+}
